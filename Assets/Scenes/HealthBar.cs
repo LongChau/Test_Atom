84972fdd97d6f82a55aac2d1342e8918f08e50d7
LongChau <duy.long@tongullman.co.jp>
2021-03-17 16:27:29 +0700

    ~ init

---
+++ Assets/Scenes/HealthBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private IntVariable Health;
+    [SerializeField]
+    private IntConstant MaxHealth;
+
+    void Update()
+    {
+        GetComponent<Image>().fillAmount = 1.0f * Health.Value / MaxHealth.Value;
+    }
+}
---
+++ Assets/Scenes/PlayerHealth.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public IntVariable Health;
+}
+
